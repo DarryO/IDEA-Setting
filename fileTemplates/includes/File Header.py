48cc69c8409698f44529be2efaba7a87b028f84e
Li Hao <haoliplus@gmail.com>
2018-08-14 15:03:13 +0800

    PY-2018.2 <hao@LH-Mac.local Create vim_settings.xml, project.default.xml, git.xml, other.xml, jdk.table.xml, Default.xml, ui.lnf.xml, laf.xml, filetypes.xml, Python Script.py, editor.codeinsight.xml, colors.scheme.xml, File Header.py, _@user_Darcula.icls, debugger.xml

---
+++ fileTemplates/includes/File Header.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+# coding: utf-8
+# -----------------------------------------------------------------------
+#     File Name           :     ${FILE_NAME}
+#     Created By          :     ${USER}
+#     Creation Date       :     [${DATE} ${HOUR}:${MINUTE}]
+#     Description         :
+# -----------------------------------------------------------------------
